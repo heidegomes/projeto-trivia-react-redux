fa9e6ea6a49ee081b5349825cb1ee6c981997876
corazon <brunogmm26@gmail.com>
2022-11-09 17:41:36 -0300

    Requisito 2 Concluído

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import logo from './trivia.png';
 import './App.css';
 import Login from './pages/Login';
+import Game from './pages/Game';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>SUA VEZ</p>
-      </header>
-      <Login />
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={ logo } className="App-logo" alt="logo" />
+          <p>SUA VEZ</p>
+        </header>
+      </div>
+
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/game" component={ Game } />
+      </Switch>
+    </>
   );
 }
